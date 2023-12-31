4e11c42dd22a5a97d0a71cd698954bde99be8313
Paz <50664967+TwoWaySix@users.noreply.github.com>
2023-12-31 19:59:10 +0100

    front end styling and esrgan simulation for testing

--- Real-ESRGAN/inference_realesrgan.py
+++ Real-ESRGAN/inference_realesrgan.py
@@ -1,1 +1,12 @@
-print("This is just a test file")
+import argparse
+import shutil
+
+print("Simulating the processor just for testing without ERSGAN")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input")
+parser.add_argument("-o", "--output")
+parser.add_argument("-n")
+args = parser.parse_args()
+
+shutil.copy(args.input, args.output)
--- static/index.js
+++ static/index.js
@@ -1,28 +1,35 @@
 document.getElementById("upload-file").onchange = async (event) => {
-    console.log("upload file")
-    const form = document.getElementById("upload-form");
-    const formData = new FormData(document.getElementById("upload-form"));
-    const response = await fetch("/api/upscale", {
-        method: "POST",
-        body: formData,
-    });
-    if (response.ok) {
-        document.getElementById(
-            "upload-info"
-        ).innerText = `Successfully uploaded ${event.target.value}`;
-        event.target.value = "";
-    }
+  console.log("upload file");
+  const form = document.getElementById("upload-form");
+  const formData = new FormData(document.getElementById("upload-form"));
+  const response = await fetch("/api/upscale", {
+    method: "POST",
+    body: formData,
+  });
+  if (response.ok) {
+    document.getElementById(
+      "upload-info"
+    ).innerText = `Successfully uploaded ${event.target.value}`;
+    event.target.value = "";
+  }
 
-    event.preventDefault();
+  event.preventDefault();
 };
 
 async function updateList() {
-    const response = await fetch("/api/upscale");
-    const images = await response.json();
+  const response = await fetch("/api/upscale");
+  const images = await response.json();
 
-    document.getElementById("processed").innerHTML = images.toSorted((a, b) => a.name > b.name)
-        .map(i => `<li>${i.name} <a href="${i.filepath}">Open</a></li>`)
-        .join("")
+  document.getElementById("processed").innerHTML = images
+    .toSorted((a, b) => a.name > b.name)
+    .map(
+      (i) => `<div>
+        <a href="${i.filepath}" target="_blank">
+            <img alt="${i.name} " src="${i.filepath}" height="150" />
+        </a> 
+        </div>`
+    )
+    .join("");
 }
 
 setInterval(updateList, 2000);
